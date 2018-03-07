ff77f03e14ef606cf3aca46667961d72f148568a
ywwynm <ywwynm@gmail.com>
2018-03-07 16:15:45 +0800

    use a naive method but surprisingly it is accepted

--- AlgorithmPractice/leetcode23_merge_k_sorted_lists.cpp
+++ AlgorithmPractice/leetcode23_merge_k_sorted_lists.cpp
@@ -12,10 +12,36 @@ struct ListNode {
 class Solution {
 public:
   ListNode * mergeKLists(vector<ListNode*>& lists) {
-
+    ListNode* head = new ListNode(0);
+    ListNode* cur = head;
+    while (true) {
+      int min_val_node_index = -1;
+      int min_val = 0x7f7f7f7f;
+      bool some_not_null = false;
+      for (int i = 0; i < lists.size(); i++) {
+        ListNode* ln = lists[i];
+        if (ln == NULL) continue;
+        some_not_null = true;
+        if (ln->val < min_val) {
+          min_val = ln->val;
+          min_val_node_index = i;
+        }
+      }
+      if (!some_not_null) break;
+      cur->next = new ListNode(min_val);
+      cur = cur->next;
+      lists[min_val_node_index] = lists[min_val_node_index]->next;
+    }
+    return head->next;
   }
 };
 
 int main() {
-
+  ListNode* one = new ListNode(1);
+  vector<ListNode*> v;
+  v.push_back(one);
+  Solution s;
+  ListNode* res = s.mergeKLists(v);
+  cout << res->val << endl;
+  return 0;
 }
