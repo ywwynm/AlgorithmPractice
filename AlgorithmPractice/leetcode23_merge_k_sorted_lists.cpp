97ec1b4c77cdd927bda1541080948115ccd8924e
ywwynm <ywwynm@gmail.com>
2018-03-07 14:34:45 +0800

    initial commit for leetcode 23: merge k sorted lists

---
+++ AlgorithmPractice/leetcode23_merge_k_sorted_lists.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+  ListNode * mergeKLists(vector<ListNode*>& lists) {
+
+  }
+};
+
+int main() {
+
+}
