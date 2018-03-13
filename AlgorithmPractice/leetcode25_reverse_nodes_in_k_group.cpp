571e617e45f459bf66803c76bc2a7a308199cd9d
ywwynm <ywwynm@gmail.com>
2018-03-13 22:04:43 +0800

    add a basic(but wrong) solution

--- AlgorithmPractice/leetcode25_reverse_nodes_in_k_group.cpp
+++ AlgorithmPractice/leetcode25_reverse_nodes_in_k_group.cpp
@@ -10,9 +10,57 @@ struct ListNode {
 
 class Solution {
 public:
+
+  void reverse2(ListNode* head, ListNode* node, ListNode* swap) {
+    if (node->next == NULL) return;
+    ListNode* a = node, *b = node->next;
+    swap = b->next;
+    b->next = a;
+    a->next = swap;
+    head->next = b;
+  }
+
+  void reverse(ListNode* head, ListNode* node, ListNode* swap, int num) {
+    // node到node后num个node进行颠倒(包括node), head->next = node
+    for (int i = 1; i <= num - 1; i++) {
+      int cur = 0;
+      ListNode* realHead = head;
+      while (cur < num - i) {
+        reverse2(realHead, node, swap);
+        cur++;
+        realHead = head->next;
+      }
+    }
+  }
+
   ListNode * reverseKGroup(ListNode* head, int k) {
     if (k == 1) return head;
-  }
+    ListNode* newHead = new ListNode(0), *swap = NULL, *cur = head, *circleBeforeHead = newHead;
+    newHead->next = head;
+    while (cur != NULL) {
+      ListNode* tmp = cur;
+      for (int i = 0; i < k - 1; i++) {
+        if (tmp->next == NULL) {
+          break; // 长度不够
+        }
+        tmp = tmp->next;
+      }
+
+      reverse(circleBeforeHead, cur, swap, k);
+
+      tmp = circleBeforeHead->next;
+      for (int i = 0; i < k - 1; i++) {
+        tmp = tmp->next;
+      }
+      circleBeforeHead = tmp;
+
+      if (circleBeforeHead != NULL) {
+        cur = circleBeforeHead->next;
+      }
+      else break;
+    }
+    return newHead->next;
+  } 
 };
 
 int main() {
@@ -26,7 +74,7 @@ int main() {
   ln3->next = ln4;
   ln4->next = ln5;
   Solution s;
-  ListNode* ln = s.reverseKGroup(ln1, 2);
+  ListNode* ln = s.reverseKGroup(ln1, 3);
 
   while (ln != NULL) {
     cout << ln->val << endl;
