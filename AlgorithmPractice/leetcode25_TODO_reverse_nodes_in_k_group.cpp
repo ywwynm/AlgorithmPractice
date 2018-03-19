f128299f16e4fa8d867d1074861d27eeb79e8297
ywwynm <ywwynm@gmail.com>
2018-03-19 19:31:55 +0800

    rename files and prepare for next solution

--- AlgorithmPractice/leetcode25_TODO_reverse_nodes_in_k_group.cpp
+++ AlgorithmPractice/leetcode25_TODO_reverse_nodes_in_k_group.cpp
@@ -83,26 +83,26 @@ public:
   } 
 };
 
-int main() {
-  ListNode* ln1 = new ListNode(1);
-  ListNode* ln2 = new ListNode(2);
-  ListNode* ln3 = new ListNode(3);
-  ListNode* ln4 = new ListNode(4);
-  ListNode* ln5 = new ListNode(5);
-  ListNode* ln6 = new ListNode(6);
-  ListNode* ln7 = new ListNode(7);
-  ListNode* ln8 = new ListNode(8);
-  ln1->next = ln2;
-  ln2->next = ln3;
-  ln3->next = ln4;
-  ln4->next = ln5;
-  ln5->next = ln6;
-  ln6->next = ln7;
-  ln7->next = ln8;
-  Solution s;
-  ListNode* ln = s.reverseKGroup(ln1, 4);
-
-  s.showList(ln);
-
-  return 0;
-}
+//int main() {
+//  ListNode* ln1 = new ListNode(1);
+//  ListNode* ln2 = new ListNode(2);
+//  ListNode* ln3 = new ListNode(3);
+//  ListNode* ln4 = new ListNode(4);
+//  ListNode* ln5 = new ListNode(5);
+//  ListNode* ln6 = new ListNode(6);
+//  ListNode* ln7 = new ListNode(7);
+//  ListNode* ln8 = new ListNode(8);
+//  ln1->next = ln2;
+//  ln2->next = ln3;
+//  ln3->next = ln4;
+//  ln4->next = ln5;
+//  ln5->next = ln6;
+//  ln6->next = ln7;
+//  ln7->next = ln8;
+//  Solution s;
+//  ListNode* ln = s.reverseKGroup(ln1, 4);
+//
+//  s.showList(ln);
+//
+//  return 0;
+//}
