a59e47c9a4ee4be9d6fa8e689077a9b577c2c1b5
ywwynm <ywwynm@github.com>
2018-12-07 19:55:46 +0800

    add solution for leetcode 60

--- AlgorithmPractice/leetcode60_permutation_sequence.cpp
+++ AlgorithmPractice/leetcode60_permutation_sequence.cpp
@@ -1,15 +1,36 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
   string getPermutation(int n, int k) {
-
+    vector<int> factorials;
+    vector<int> numbers;
+    int accumulate = 1;
+    for (int i = 1; i <= n; i++) {
+      accumulate *= i;
+      factorials.push_back(accumulate);
+      numbers.push_back(i);
+    }
+    string ret = "";
+    k--;
+    for (int i = 1; i < n; i++) {
+      int fac = factorials[n - i - 1];
+      int idx = k / fac;
+      ret += ('0' + numbers[idx]);
+      numbers.erase(numbers.begin() + idx);
+      k -= idx * fac;
+    }
+    ret += ('0' + numbers[0]);
+    return ret;
   }
 };
 
 int main() {
-
+  Solution s;
+  cout << s.getPermutation(2, 2) << endl;
   return 0;
 }
