ae7434149b6ccff91d710a0d605e09667ba155e4
ywwynm <ywwynm@github.com>
2019-01-09 17:14:35 +0800

    add passed solution for leetcode 74

--- AlgorithmPractice/leetcode74_search_a_2d_matrix.cpp
+++ AlgorithmPractice/leetcode74_search_a_2d_matrix.cpp
@@ -23,15 +23,34 @@ public:
     return false;
   }
 
+  bool searchCol(vector<vector<int>> matrix, int target) {
+    const int row = matrix.size();
+    int start = 0, end = row - 1;
+    int mid = (start + end) / 2;
+    while (start <= end) {
+      if (matrix[mid][0] == target) {
+        return true;
+      } else if (matrix[mid][0] > target) {
+        end = mid - 1;
+      } else {
+        start = mid + 1;
+      }
+      mid = (start + end) / 2;
+      cout << "searchCol: " << start << ", " << end << ", " << mid << endl;
+    }
+    return false;
+  }
+
   bool searchMatrix(vector<vector<int>>& matrix, int target) {
     const int row = matrix.size();
     if (row == 0) return false;
     if (row == 1) return searchRow(matrix[0], target);
     const int col = matrix[0].size();
     if (col == 0) return false;
+    if (col == 1) return searchCol(matrix, target);
     int start = 0, end = row - 1;
     int mid = (start + end) / 2;
-    while (start <= mid) {
+    while (start <= end) {
       cout << "searchMatrix: " << start << ", " << end << ", " << mid << endl;
       int elemFirst = matrix[mid][0];
       if (elemFirst == target) {
@@ -72,11 +91,41 @@ int main() {
   Solution s;
   cout << s.searchMatrix(matrix, 26) << endl;*/
 
-  int row1[] = { 1 };
+  //int row1[] = { 1 };
+  //vector<int> v_row1(row1, row1 + 1);
+  //vector<vector<int>> matrix;
+  //// matrix.push_back(v_row1);
+  //Solution s;
+  //cout << s.searchMatrix(matrix, 0) << endl;
+
+  /*int row1[] = { 1 };
+  int row2[] = { 10 };
+  int row3[] = { 23 };
+  int row4[] = { 31 };
+  int row5[] = { 32 };
   vector<int> v_row1(row1, row1 + 1);
+  vector<int> v_row2(row2, row2 + 1);
+  vector<int> v_row3(row3, row3 + 1);
+  vector<int> v_row4(row4, row4 + 1);
+  vector<int> v_row5(row5, row5 + 1);
   vector<vector<int>> matrix;
-  // matrix.push_back(v_row1);
+  matrix.push_back(v_row1);
+  matrix.push_back(v_row2);
+  matrix.push_back(v_row3);
+  matrix.push_back(v_row4);
+  matrix.push_back(v_row5);
   Solution s;
-  cout << s.searchMatrix(matrix, 0) << endl;
+  cout << s.searchMatrix(matrix, 31) << endl;*/
+
+  int row1[] = { 1,1 };
+  int row2[] = { 2,2 };
+  vector<int> v_row1(row1, row1 + 2);
+  vector<int> v_row2(row2, row2 + 2);
+  vector<vector<int>> matrix;
+  matrix.push_back(v_row1);
+  matrix.push_back(v_row2);
+  Solution s;
+  cout << s.searchMatrix(matrix, 1) << endl;
+
   return 0;
 }
