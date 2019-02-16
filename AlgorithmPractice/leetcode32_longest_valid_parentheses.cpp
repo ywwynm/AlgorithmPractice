073e35b65d917801b52e69d708d527cd524c5175
ywwynm <ywwynm@github.com>
2019-02-16 17:20:51 +0800

    a wrong answer

--- AlgorithmPractice/leetcode32_longest_valid_parentheses.cpp
+++ AlgorithmPractice/leetcode32_longest_valid_parentheses.cpp
@@ -1,15 +1,33 @@
 #include <iostream>
+#include <stack>
 
 using namespace std;
 
 class Solution {
 public:
   int longestValidParentheses(string s) {
-
+    if (s.empty()) return 0;
+    stack<char> st;
+    st.push(s[0]);
+    int count = 0;
+    for (int i = 1; i < s.length(); i++) {
+      char c = s[i];
+      if (st.empty()) {
+        st.push(c);
+      } else if (st.top() == '(' && c == ')') {
+        count += 2;
+        st.pop();
+      } else {
+        st.push(c);
+      }
+    }
+    return count;
   }
 };
 
 int main() {
-
+  Solution s;
+  // ()(() should return 2
+  cout << s.longestValidParentheses("()(()") << endl;
   return 0;
 }
